26f3ad64f9a2d759f6704f23889763f7cedf5bb4
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2023-04-11 12:07:30 +0300

    feat: date tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -6,7 +6,6 @@
  *                                                                                           *
  ******************************************************************************************* */
 
-
 /**
  * Parses a rfc2822 string date representation into date value
  * For rfc2822 date specification refer to : http://tools.ietf.org/html/rfc2822#page-14
@@ -19,8 +18,29 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  const date = new Date(value);
+  if (isNaN(date)) {
+    // Якщо отриманий недійсний об'єкт дати, спробуємо вручну розпарсити рядок
+    const parts = value.split(' ');
+    const day = parts[1].replace(',', '');
+    const month = parts[2];
+    const year = parts[3];
+    const time = parts[4].split(':');
+    const hours = time[0];
+    const minutes = time[1];
+    const seconds = time[2];
+    const timezone = parts[5];
+
+    // Перевіряємо, чи всі необхідні значення вдалося розпарсити
+    if (day && month && year && hours && minutes && seconds && timezone) {
+      return new Date(`${day} ${month} ${year} ${hours}:${minutes}:${seconds} ${timezone}`);
+    } else {
+      throw new Error('Invalid date format');
+    }
+  } else {
+    return date;
+  }
 }
 
 /**
@@ -34,10 +54,34 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
-}
+function parseDataFromIso8601(value) {
+  const date = new Date(value);
+  if (isNaN(date)) {
+    // Якщо отриманий недійсний об'єкт дати, спробуємо вручну розпарсити рядок
+    const parts = value.split('T');
+    const dateString = parts[0];
+    const timeString = parts[1].replace('Z', '').split('+')[0];
+    const timezoneString = parts[1].split('+')[1] || '';
 
+    // Перевіряємо, чи всі необхідні значення вдалося розпарсити
+    if (dateString && timeString) {
+      const year = dateString.slice(0, 4);
+      const month = dateString.slice(5, 7);
+      const day = dateString.slice(8, 10);
+      const hours = timeString.slice(0, 2);
+      const minutes = timeString.slice(3, 5);
+      const seconds = timeString.slice(6, 8);
+      const timezoneHours = timezoneString.slice(0, 2);
+      const timezoneMinutes = timezoneString.slice(3, 5);
+
+      return new Date(Date.UTC(year, month - 1, day, hours, minutes, seconds) + (timezoneHours * 60 + timezoneMinutes) * 60000);
+    } else {
+      throw new Error('Invalid date format');
+    }
+  } else {
+    return date;
+  }
+}
 
 /**
  * Returns true if specified date is leap year and false otherwise
@@ -53,10 +97,10 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 }
-
 
 /**
  * Returns the string representation of the timespan between two dates.
@@ -73,10 +117,14 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const timeDifference = endDate - startDate;
+  const hours = Math.floor(timeDifference / (3600 * 1000));
+  const minutes = Math.floor((timeDifference % (3600 * 1000)) / (60 * 1000));
+  const seconds = Math.floor((timeDifference % (60 * 1000)) / 1000);
+  const milliseconds = timeDifference % 1000;
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}.${milliseconds.toString().padStart(3, '0')}`;
 }
-
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock
@@ -94,10 +142,17 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
-}
+function angleBetweenClockHands(date) {
+  const hours = date.getUTCHours();
+  const mins = date.getUTCMinutes();
 
+  let angle = Math.abs((hours % 12) * 30 + mins / 2 - mins * 6);
+  while (angle > 180) {
+    angle = 360 - angle;
+  }
+
+  return (angle / 180) * Math.PI;
+}
 
 module.exports = {
   parseDataFromRfc2822,
